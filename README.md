7878c7de794511d7b70e97fc74a4a88fc49b149b
jtvaug227 <jtvaughan227@gmail.com>
2026-06-08 19:54:02 -0400

    Audio is more than just noise
    Finally fixed the issue with there being high-pitched whines in the
    middle of the audio track. It was probably me double-implementing the
    fact that the apu cycles only 1 per 2 cpu cycles.
    Reimplemented the pulse generators and their sequencers.
    Pulse generators now have length counters that mute respective channels.
    Frame counter + modes now exist and clock the length counters.
    SMB sounds like SMB...

--- README.md
+++ README.md
@@ -1,31 +1,24 @@
 # nes_rust
 ## What is it
-An NES emulator written in rust! Well, it might be one day. Right now, it's actually an emulator of an NES-6502 microprocessor.  
+An NES emulator written in rust!
+It's specifically a library for the NMOS 6502 processor and other components used to compose the NES. The
+actual NES behavior comes from the runrom example where the components are interconnected.
 
 This started as a learning project back in 2022 when I was first learning the rust language; it was a mess of tutorial code and 
 some rust technologies I had just picked up. I have since revived the project with new goals and motiviation to make a complete product.  
 
-## Project Goals
-- Half-cycle instruction emulation,
-- Implements all 256 opcodes for the NES-6502 along with its bugs,
-- Can play SMB3
-
 ## Overview
-A lot of emulators I have referenced like to emulate entire instructions in a single call and make IO calls to an internally owned 'BUS'. 
-While this is alright if just emulating the 6502 microprocessor, it would be a pain to time IO calls and indicate dummy reads/writes.  
+This emulation of the NMOS 6502 is focused on half-cycle timing - splitting up the processing of instructions into high and low portions of the clock cycle.
+I chose this level of accuracy because it should be able to emulate most of the instructions perfectly as well as the irregularities present in the original
+silicon.  
+
+A lot of emulators I have referenced like to emulate entire instructions in a single call and make IO calls to an internally owned/shared 'BUS' at instant speed. 
+The problem with single-call instructions is that they lose a lot of the nuance of cycle timings and emulators have to do a lot of work to ensure that unexpected
+events like interrupts and IO are handled properly. This level of emulation also generally couples all the components in a monolithic structure.
 
 Emulating instructions by the cycle solves most problems with the read/write timings with IO, but I was looking into how cartridges work with
 supplying ROM/RAM as well as cycle timings and I came across the article [A new cycle-stepped 6502 CPU emulator](https://floooh.github.io/2019/12/13/cycle-stepped-6502.html)
-by Brain Dump. Here, I saw how the buses were being treated just as a mutable integer representing the pins of the chip.
-
-The idea of the buses being parameterized integers combined with a few posts I read between the Nesdev wiki and forums talking about how the cpu times itself with the bus using
-the phi 0, 1, and 2 cycles. I knew I wanted to emulate the cpu <-> bus interactions correctly using half-cycle timings.
-
-Using 1/2 cycles with an external bus found the benfits that my cpu struct became plain-old-data that was easily copyable, as well as
-the instructions becoming increasingly repetitive in nature. Another benefit was that the bus is now a part of the higher picture that users 
-can configure as needed - e.g. I found it made grabbing a copy of the cpu at the start of instructions for debugging purposes a simple task. Listening in on 
-bus reads/writes is also as easy as just inserting a print statement after each 1/2 cycle, phi1 for the address+read byte, and phi2 for the byte written.
-No need for pass callback functions or modifying a deeply embedded read/write function.
+by Brain Dump. Inspiration was taken from the idea of passing the 'pins' of the chip as a refernce to some POD.
 
 ```rust
 fn my_clock(&mut self) {
@@ -34,7 +27,7 @@ fn my_clock(&mut self) {
   let data_bus = 0;
 
   let phi = false; // phi1
-  cpu.clock(&mut address_bus, &mut data_bus, &mut rw_pin, phi1);
+  cpu.clock(&mut address_bus, &mut data_bus, &mut rw_pin, phi);
 
   if rw_pin {
     // Cpu is configured to read,
@@ -42,7 +35,7 @@ fn my_clock(&mut self) {
   }
 
   let phi = true; // phi2
-  cpu.clock(&mut address_bus, &mut data_bus, &mut rw_pin, phi1);
+  cpu.clock(&mut address_bus, &mut data_bus, &mut rw_pin, phi);
   if !rw_pin {
     // Cpu is configured to write
     // expect valid data and address
@@ -50,13 +43,21 @@ fn my_clock(&mut self) {
 }
 ```
 
+## Project Goals
+- [ ] Half-cycle instruction emulation
+- [ ] Implements all 256 opcodes for the NES' 6502 along with its bugs
+- [ ] Can play SMB3
+- [ ] Audio
+- [ ] OpenBus behavior
+- [ ] The most popular mappers
+- [ ] Official desktop app (Separate library)
+
 ## Immediate Goals
-- Try using u8::overflowing_add to get overflows as an immediate result.
-- Finish implementing opcodes (addressing should be complete minus yet-to-be-caught/verified bugs)
-- Splitting cycle handling into clock_low() and clock_high() rather than relying on a phi boolean
-- Finish decoding Cartidge data
-- More tests, finishing tests, semi-automating tests.
-- Separate project with TUI for better display.
+- [x] ~~Try using u8::overflowing_add to get overflows as an immediate result.~~ Should now be a feature flag.
+- [x] ~~Finish implementing opcodes~~ All opcodes are implemented, though some improperly in terms of the openbus.
+- [ ] Splitting cycle handling into clock_low() and clock_high() rather than relying on a phi boolean
+- [ ] Finish decoding Cartidge data
+- [ ] Official testing infrastrucutre and semi-automating testing.
 
 ## References
 NESDev wiki: https://www.nesdev.org  
--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,6 +1,4 @@
 struct Sequencer {
-    sequence: u32,
-    new_sequence: u32,
     timer: u16,
     reload: u16,
     output: u8,
@@ -9,28 +7,173 @@ struct Sequencer {
 impl Sequencer {
     fn new() -> Self {
         Self {
-            sequence: 0,
-            new_sequence: 0,
             timer: 0,
             reload: 0,
             output: 0,
         }
     }
-    fn clock<T: Fn(&mut u32)>(&mut self, enabled: bool, seq_fn: T) -> u8 {
-        if enabled {
-            self.timer = if self.timer == 0 {
-                seq_fn(&mut self.sequence);
-                self.output = (self.sequence & 0x00000001) as u8;
-                self.reload + 1
-            } else {
-                self.timer - 1
-            };
-        }
-
-        self.output
+    fn clock(&mut self) {
+        self.timer = if self.timer == 0 {
+            self.output = if self.output == 0 { 7 } else { self.output - 1 };
+            self.reload
+        } else {
+            self.timer - 1
+        };
     }
 }
 
+struct LengthCounter(u8);
+impl LengthCounter {
+    pub fn new() -> Self {
+        Self(0)
+    }
+    pub fn clock(&mut self, enabled: bool, halt: bool) {
+        // This could possibly be an array lookup
+        // index = enabled | halt
+        // a = [self.0, self.0, 1, 0]
+        // saturating_sub(self.0, a[index])
+        self.0 = if enabled {
+            if halt {
+                self.0
+            } else {
+                u8::saturating_sub(self.0, 1)
+            }
+        } else {
+            0
+        };
+    }
+    pub fn reload(&mut self, value: u8) {
+        let linear = value & 0b1 > 0;
+        let value = value >> 1;
+        self.0 = match (linear, value) {
+            (true, v) => match v {
+                0b1111 => 30,
+                0b1110 => 28,
+                0b1101 => 26,
+                0b1100 => 24,
+                0b1011 => 22,
+                0b1010 => 20,
+                0b1001 => 18,
+                0b1000 => 16,
+
+                0b0111 => 14,
+                0b0110 => 12,
+                0b0101 => 10,
+                0b0100 => 8,
+                0b0011 => 6,
+                0b0010 => 4,
+                0b0001 => 2,
+                0b0000 => 254,
+                _ => 0,
+            },
+            (false, v) => match v {
+                0b1111 => 32,
+                0b1110 => 16,
+                0b1101 => 72,
+                0b1100 => 192,
+                0b1011 => 96,
+                0b1010 => 48,
+                0b1001 => 24,
+                0b1000 => 12,
+
+                0b0111 => 26,
+                0b0110 => 14,
+                0b0101 => 60,
+                0b0100 => 160,
+                0b0011 => 80,
+                0b0010 => 40,
+                0b0001 => 20,
+                0b0000 => 10,
+                _ => 0,
+            },
+        };
+    }
+    pub fn value(&self) -> u8 {
+        self.0
+    }
+}
+
+struct Sweeper {}
+
+struct PulseChannel {
+    duty_cycle: u8,
+    length_counter_toggle: bool,
+    volume_envelope_toggle: bool,
+    volume_envelope_period: u8,
+    sweeper: Sweeper,
+    sequencer: Sequencer,
+    length_counter: LengthCounter,
+}
+
+impl PulseChannel {
+    pub fn new() -> Self {
+        Self {
+            duty_cycle: 0,
+            length_counter_toggle: false,
+            volume_envelope_toggle: false,
+            volume_envelope_period: 0,
+            sweeper: Sweeper {},
+            sequencer: Sequencer::new(),
+            length_counter: LengthCounter::new(),
+        }
+    }
+
+    pub fn clock(&mut self, enabled: bool) {
+        if enabled {
+            self.sequencer.clock();
+        }
+    }
+
+    pub fn sample(&self) -> u8 {
+        const SEQ_OUTPUTS: [[u8; 8]; 4] = [
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0],
+        ];
+        // const PULSE_SEQ_DEFAULTS: [f64; 4] = [0.125, 0.25, 0.5, 0.75];
+        let frequency = self.duty_cycle as usize;
+        let index = self.sequencer.output as usize;
+        let sample = SEQ_OUTPUTS[frequency][index];
+        let muted = self.length_counter.value() == 0;
+        if muted {
+            0
+        } else {
+            sample
+        }
+    }
+
+    pub fn write_0(
+        &mut self,
+        duty: u8,
+        envelope_loop_length_counter_enable: u8,
+        volume_envelope_toggle: u8,
+        volume_envelope_period: u8,
+    ) {
+        self.duty_cycle = duty;
+        self.length_counter_toggle = envelope_loop_length_counter_enable > 0;
+        self.volume_envelope_toggle = volume_envelope_toggle > 0;
+        self.volume_envelope_period = volume_envelope_period;
+
+        // Changes duty cycle without resetting sequencer
+    }
+
+    pub fn write_1(&mut self, enabled: bool, negate: bool, period: u8, shift_count: u8) {}
+    pub fn write_2(&mut self, timer_low: u8) {
+        self.sequencer.reload = (self.sequencer.reload & 0xFF00) | timer_low as u16;
+    }
+    pub fn write_3(&mut self, length_counter_load: u8, timer_hi: u8) {
+        self.sequencer.reload = (self.sequencer.reload & 0x00FF) | ((timer_hi as u16) << 8);
+        self.sequencer.timer = self.sequencer.reload;
+        self.sequencer.output = 0;
+
+        self.length_counter.reload(length_counter_load);
+
+        // Immediately restart sequencer, restart envelope. Period Divider is NOT reset
+    }
+}
+
+// Some alternative implementation
 struct PulseOscillator {
     frequency: f64,
     duty_cycle: f64,
@@ -50,8 +193,10 @@ impl PulseOscillator {
         }
     }
     fn sample(&mut self, enabled: bool, t: f64) -> f64 {
-        if !enabled { return self.prev_sample; }
-        const TWO_PI: f64 = 2.0 * std::f64::consts::PI;
+        if !enabled {
+            return self.prev_sample;
+        }
+        const TWO_PI: f64 = std::f64::consts::TAU;
         let mut a: f64 = 0.0;
         let mut b: f64 = 0.0;
         let p = self.duty_cycle * TWO_PI;
@@ -70,7 +215,10 @@ impl PulseOscillator {
         self.prev_sample = (2.0 * self.amplitude / std::f64::consts::PI) * (a - b);
         self.prev_sample
     }
-    fn sample_weird(&self, t: f64) -> f64 {
+    fn sample_weird(&mut self, enabled: bool, t: f64) -> f64 {
+        if !enabled {
+            return self.prev_sample;
+        }
         const PI: f64 = std::f64::consts::PI;
         let mut a: f64 = 0.0;
         let mut b: f64 = 0.0;
@@ -78,7 +226,6 @@ impl PulseOscillator {
         let sin = |s: f64| {
             let j = (s * 0.15915).fract();
             20.785 * j * (j - 0.5) * (j - 1.0)
-            // s.sin()
         };
         for n in 1..self.harmonics {
             let n = 2 * n - 1;
@@ -87,7 +234,8 @@ impl PulseOscillator {
             a += -sin(c) / n_f;
             b += -sin(c - (p * n_f)) / n_f;
         }
-        (4.0 * self.amplitude / std::f64::consts::PI) * (a - b -self.duty_cycle*PI/2.0)
+        self.prev_sample = (2.0 * self.amplitude / PI) * (a - b - self.duty_cycle * PI / 2.0);
+        self.prev_sample
     }
 }
 
@@ -108,10 +256,8 @@ impl ApuPinout {
 }
 
 pub struct Apu {
-    pulse1: PulseOscillator,
-    square1: Sequencer,
-    pulse2: PulseOscillator,
-    square2: Sequencer,
+    square1: PulseChannel,
+    square2: PulseChannel,
     triangle: Sequencer,
     noise: Sequencer,
 
@@ -121,17 +267,16 @@ pub struct Apu {
     noise_enable: bool,
 
     emulated_time: f64,
-    clock_counter: u32,
-    frame_clock_counter: u32,
+    clock_counter: u64,
+    frame_counter: u32,
+    frame_mode: bool,
 }
 
 impl Apu {
     pub fn new() -> Self {
         Self {
-            pulse1: PulseOscillator::new(),
-            square1: Sequencer::new(),
-            pulse2: PulseOscillator::new(),
-            square2: Sequencer::new(),
+            square1: PulseChannel::new(),
+            square2: PulseChannel::new(),
             triangle: Sequencer::new(),
             noise: Sequencer::new(),
 
@@ -142,7 +287,8 @@ impl Apu {
 
             emulated_time: 0.0,
             clock_counter: 0,
-            frame_clock_counter: 0,
+            frame_counter: 0,
+            frame_mode: false,
         }
     }
     pub fn clock(&mut self, pins: &mut ApuPinout) -> f64 {
@@ -153,45 +299,58 @@ impl Apu {
                 // DDLC NNNN
                 0x00 => {
                     let duty = (data & 0b11000000) >> 6;
-                    let _loop_evelope = (data & 0b00100000) >> 5;
-                    let _constant_volume = (data & 0b00010000) >> 4;
-                    let _envelope_period = data & 0b00001111;
-                    const SEQ_DEFAULTS: [u32; 4] = [0b00000001, 0b00000011, 0b00001111, 0b11111100];
-                    const PULSE_SEQ_DEFAULTS: [f64; 4] = [0.125, 0.25, 0.5, 0.75];
-                    self.square1.sequence = SEQ_DEFAULTS[duty as usize];
-                    self.pulse1.duty_cycle = PULSE_SEQ_DEFAULTS[duty as usize];
+                    let loop_envelope = (data & 0b00100000) >> 5;
+                    let volume_envelope_select = (data & 0b00010000) >> 4;
+                    let volume_envelope_data = data & 0b00001111;
+                    self.square1.write_0(
+                        duty,
+                        loop_envelope,
+                        volume_envelope_select,
+                        volume_envelope_data,
+                    );
                 }
-                0x01 => {}
+                0x01 => {
+                    let enabled = (0x80 & data) > 0;
+                    let period = (0b01110000 & data) >> 4;
+                    let negate = (0b00001000 & data) > 0;
+                    let shift_count = 0b111 & data;
+                    self.square1.write_1(enabled, negate, period, shift_count);
+                }
                 0x02 => {
-                    self.square1.reload = (self.square1.reload & 0xFF00) | data as u16;
+                    self.square1.write_2(data);
                 }
                 0x03 => {
                     let timer_hi = data & 0b111;
-                    let reload = (data & 0b11111000) >> 3;
-                    self.square1.reload = (self.square1.reload & 0x00FF) | ((timer_hi as u16) << 8);
-                    // println!("Setting P1 reload to {}", self.square1.reload);
-                    self.square1.timer = self.square1.reload;
+                    let length_counter_load = (data & 0b11111000) >> 3;
+                    self.square1.write_3(length_counter_load, timer_hi);
                 }
                 // Pulse 2
                 0x04 => {
                     let duty = (data & 0b11000000) >> 6;
-                    let _loop_evelope = (data & 0b00100000) >> 5;
-                    let _constant_volume = (data & 0b00010000) >> 4;
-                    let _envelope_period = data & 0b00001111;
-                    const SEQ_DEFAULTS: [u32; 4] = [0b00000001, 0b00000011, 0b00001111, 0b11111100];
-                    const PULSE_SEQ_DEFAULTS: [f64; 4] = [0.125, 0.25, 0.5, 0.75];
-                    self.square2.sequence = SEQ_DEFAULTS[duty as usize];
-                    self.pulse2.duty_cycle = PULSE_SEQ_DEFAULTS[duty as usize];
+                    let loop_envelope = (data & 0b00100000) >> 5;
+                    let volume_envelope_select = (data & 0b00010000) >> 4;
+                    let volume_envelope_data = data & 0b00001111;
+                    self.square2.write_0(
+                        duty,
+                        loop_envelope,
+                        volume_envelope_select,
+                        volume_envelope_data,
+                    );
                 }
-                0x05 => {}
+                0x05 => {
+                    let enabled = (0x80 & data) > 0;
+                    let period = (0b01110000 & data) >> 4;
+                    let negate = (0b00001000 & data) > 0;
+                    let shift_count = 0b111 & data;
+                    self.square2.write_1(enabled, negate, period, shift_count);
+                }
                 0x06 => {
-                    self.square2.reload = (self.square2.reload & 0xFF00) | data as u16;
+                    self.square2.write_2(data);
                 }
                 0x07 => {
                     let timer_hi = data & 0b111;
-                    let reload = (data & 0b11111000) >> 3;
-                    self.square2.reload = (self.square2.reload & 0x00FF) | ((timer_hi as u16) << 8);
-                    self.square2.timer = self.square2.reload;
+                    let length_counter_load = (data & 0b11111000) >> 3;
+                    self.square2.write_3(length_counter_load, timer_hi);
                 }
                 // Triangle
                 0x08 => {}
@@ -214,72 +373,117 @@ impl Apu {
                     self.square2_enable = (data & 0x02) > 0;
                     self.triangle_enable = (data & 0x04) > 0;
                     self.noise_enable = (data & 0x08) > 0;
+                    // TODO: Clear dmc irq (same for read)
+                }
+                0x17 => {
+                    self.frame_mode = (data & 0x80) > 0;
+                    let irq_inhibit_flag = (data & 0x40) > 0;
+                    // TODO: Reset frame counter on the next odd cycle following the next even cycle
+                    // (3~4 CPU cycles)
                 }
                 _ => {}
             }
         } else if pins.cpu_rw && pins.cpu_addr == 0x17 {
             // Put status on data bus
+            let p1 = (self.square1_enable as u8);
+            let p2 = (self.square2_enable as u8) << 1;
+            let t = (self.triangle_enable as u8) << 2;
+            let n = (self.noise_enable as u8) << 3;
+
+            pins.cpu_data = p1 | p2 | t | n;
+
+            // clears frame interrupt flag
+            // does not clear dmc interrupt flag
         }
 
         self.emulated_time += 1.0 / 1789773.0;
 
         let mut acc_sample = 0.0;
         // APU Timer clocks every 2 cpu cycles
-        if self.clock_counter % 2 == 0 {
-            self.frame_clock_counter += 1;
+        // TODO: This even-cycle check was removed and seems to
+        // fix the high-pitch whining. I believe I was double-checking even cycles and therefore only
+        // generating 1/2 the samples needed.
+        // if self.clock_counter % 2 == 1 {
+            // self.clock_counter = 0;
+            self.frame_counter += 1;
 
-            if self.frame_clock_counter == 3729 {
-                // quarter frame clock
+            let mode = false;
+
+            // TODO: Seems like frame cycle 0 and MAX_CYCLE are the same cycle,
+            // and resetting the frame cycle to 0 will trigger the quarter and half frames
+            // This might require evaluating/resetting the frame counter early on
+            let (clock_envelopes, clock_length_counters, pulls_irq) = match mode {
+                false => (
+                    matches!(self.frame_counter, 3728 | 7456 | 11185 | 14914),
+                    matches!(self.frame_counter, 7456 | 14914),
+                    matches!(self.frame_counter, 14914),
+                ),
+                true => (
+                    matches!(self.frame_counter, 3728 | 7456 | 11185 | 18640),
+                    matches!(self.frame_counter, 7456 | 18640),
+                    matches!(self.frame_counter, 18640),
+                ),
+            };
+
+            if clock_envelopes {
+                // Clock envelopes and triangle linear counter
             }
 
-            if self.frame_clock_counter == 7457 {
-                // quarter & half frame clock
+            if clock_length_counters {
+                // Clock linear counters and sweep units
+                self.square1
+                    .length_counter
+                    .clock(self.square1_enable, self.square1.length_counter_toggle);
+                self.square2
+                    .length_counter
+                    .clock(self.square2_enable, self.square2.length_counter_toggle);
             }
 
-            if self.frame_clock_counter == 11186 {
-                // quarter frame clock
+            if pulls_irq {
+                self.frame_counter = 0;
+                if mode {
+                    // pull irq on cpu, do not reset until status is written
+                }
             }
+        // }
 
-            if self.frame_clock_counter == 14916 {
-                // quarter & half frame clock
-                // reset frame_clock_counter
-            }
+        self.square1.clock(self.square1_enable);
+        let p1_sample = self.square1.sample() as f64;
 
-            // if quarter frame ...
-            //
-            // if half frame ...
+        self.square2.clock(self.square2_enable);
+        let p2_sample = self.square2.sample() as f64;
 
-            self.square1.clock(self.square1_enable, |s| { *s = s.rotate_right(1); });
-            let p1_sample = self.square1.output as f64;
-            self.pulse1.frequency = 1789773.0 / (16.0 * (self.square1.reload + 1) as f64);
-            let p1_sample = self.pulse1.sample(self.square1_enable, self.emulated_time);
+        // self.triangle.clock(self.triangle_enable, |_s| {});
+        let t_sample = 0.0;
 
-            self.square2.clock(self.square2_enable, |s| { *s = s.rotate_right(1); });
-            let p2_sample = self.square1.output as f64;
-            self.pulse2.frequency = 1789773.0 / (16.0 * (self.square2.reload + 1) as f64);
-            let p2_sample = self.pulse2.sample(self.square2_enable, self.emulated_time);
+        // self.noise.clock(self.noise_enable, |_s| {});
+        let n_sample = 0.0;
 
-            self.triangle.clock(self.triangle_enable, |_s| {});
-            let t_sample = 0.0;
+        let dmc_sample = 0.0;
 
-            self.noise.clock(self.noise_enable, |_s| {});
-            let n_sample = 0.0;
+        let pulse_under = p1_sample + p2_sample;
+        let pulse_out = if pulse_under == 0.0 {
+            0.0
+        } else {
+            95.88 / ((8128.0 / pulse_under) + 100.0)
+        };
+        let tnd_under = (t_sample / 8227.0) + (n_sample / 12241.0) + dmc_sample / 22638.0;
+        let tnd_out = if tnd_under == 0.0 {
+            0.0
+        } else {
+            159.79 / (1.0 / tnd_under + 100.0)
+        };
 
-            let dmc_sample = 0.0;
-
-            // let pulse_under = p1_sample*15.0 + p2_sample*15.0;
-            // let pulse_out = if pulse_under == 0.0 { 0.0 } else { 95.88 / ((8128.0 / pulse_under) + 100.0) };
-            // let tnd_under = (t_sample / 8227.0) + (n_sample / 12241.0) + dmc_sample / 22638.0;
-            // let tnd_out = if tnd_under == 0.0 { 0.0 } else { 159.79 / (1.0/tnd_under + 100.0) };
-
-            acc_sample += p1_sample + p2_sample;
-        }
+        acc_sample += pulse_out + tnd_out;
 
         self.clock_counter += 1;
 
-        // let square1_sample = self.square1.output as f64;
-        // let square2_sample = self.square2.output as f64;
-
         acc_sample
+    }
+}
+
+impl Default for Apu {
+    fn default() -> Self {
+        Self::new()
     }
 }
